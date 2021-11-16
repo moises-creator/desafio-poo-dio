c452e738a47cb3ee1a8671b44a785e6585339eaa
Moises <moises.ximenes5@gmail.com>
2021-11-16 15:27:24 -0300

    first commit

---
+++ src/main/java/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,19 @@
+package br.com.dio.desafio.dominio;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descricao curso js");
+        curso2.setCargaHoraria(8);
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+    }
+}
